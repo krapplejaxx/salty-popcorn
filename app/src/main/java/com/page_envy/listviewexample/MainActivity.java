fa872121a9151366e2cf93809e10e26a21637f2a
Page <nvpage@hub-289-imac1.office.at.ufl.edu>
2014-11-20 15:02:08 -0500

    Initial commit

---
+++ app/src/main/java/com/page_envy/listviewexample/MainActivity.java
@@ -0,0 +1,68 @@
+package com.page_envy.listviewexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] favoriteTVShows = {"Pushing Dasies", "Better Off Ted",
+                "Twin Peaks", "Freaks and Geeks", "Orphan Black", "Walking Dead",
+                "Breaking Bad", "The 400", "Alphas", "Life on Mars"};
+
+        ListAdapter theAdapter = new myAdapter(this, favoriteTVShows);
+
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                String tvShowPicked = "You selected " +
+                        String.valueOf(adapterView.getItemAtPosition(position));
+
+
+                Toast.makeText(MainActivity.this, tvShowPicked, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/page_envy/listviewexample/myAdapter.java
@@ -0,0 +1,41 @@
+package com.page_envy.listviewexample;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.page_envy.listviewexample.R;
+
+class myAdapter extends ArrayAdapter<String>{
+
+
+    public myAdapter(Context context, String[] values) {
+        super(context, R.layout.row_layout2, values);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater theInflater = LayoutInflater.from(getContext());
+
+        View theView = theInflater.inflate(R.layout.row_layout2,
+                parent, false);
+
+        String tvShow = getItem(position);
+
+        TextView theTextView = (TextView) theView.findViewById(R.id.textView1);
+
+        theTextView.setText(tvShow);
+
+        ImageView theImageView = (ImageView) theView.findViewById(R.id.theImageView1);
+
+        theImageView.setImageResource(R.drawable.dot);
+
+        return theView;
+
+    }
+}
